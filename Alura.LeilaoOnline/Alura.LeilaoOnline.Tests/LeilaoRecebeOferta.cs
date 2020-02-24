dced8073fbe20941a3de5ca117ff8b6f8bdaa62e
carlos roberto cardoso dos reis bueno <ccarlosbueno@outlook.com>
2020-02-24 12:58:14 -0300

    include project

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoRecebeOferta.cs
@@ -0,0 +1,32 @@
+﻿using Alura.LeilaoOnline.Core.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+	public class LeilaoRecebeOferta
+	{
+		[Fact]
+		public void IgnoraNovoLancaDadoPregaoFinalizado() {
+			var leilao = new Leilao("Quadro Alejadinho");
+			var fulano = new Interessada("Fulano", leilao);
+			var ciclano = new Interessada("Ciclano", leilao);
+
+
+			leilao.RecebeLance(fulano, 100);
+			leilao.RecebeLance(ciclano, 300);
+			leilao.RecebeLance(ciclano, 600);
+			
+
+			leilao.TerminaPregao();
+
+			leilao.RecebeLance(fulano, 800);
+
+			var valorEsperado = 800;
+			var valorRecebido = leilao.Ganhador.Valor;
+			Assert.Equal(valorEsperado, valorRecebido);
+		}
+	}
+}
